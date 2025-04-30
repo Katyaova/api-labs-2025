30fe489a9571e4ef4cd17058addbb6090534a570
Katyaova <puhhova@hotmail.com>
2025-04-30 09:59:36 +0100

    Added Users Router

---
+++ tasky-api/api/users/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import User from './userModel.js'; 
+
+const router = express.Router();
+
+// Get all users
+router.get('/', async (req, res) => {
+    const users = await User.find();
+    res.status(200).json(users);
+});
+
+// Register (create) a user
+router.post('/', async (req, res) => {
+    await User(req.body).save();
+    res.status(201).json({
+        code: 201,
+        msg: 'Successfully created new user.',
+    });
+});
+
+export default router;
--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,32 +1,37 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import tasksRouter from './api/tasks/index.js'; // Remember the ./api/tasks/index.js
+import tasksRouter from './api/tasks/index.js';
+import usersRouter from './api/users/index.js';
 import './db';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+// Enable CORS
+app.use(cors());
+
+// Parse JSON
+app.use(express.json());
+
 // eslint-disable-next-line no-unused-vars
 const errHandler = (err, req, res, next) => {
-  /* If the error is in development, show full error,
-     if in production, only show a simple message */
   if (process.env.NODE_ENV === 'production') {
     return res.status(500).send('Something went wrong!');
   }
   res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack}`);
 };
 
-
-// 👇 Add this middleware to parse JSON
-app.use(express.json());
-
+// Routers
 app.use('/api/tasks', tasksRouter);
+app.use('/api/users', usersRouter);
 
+// Start server
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
 
+// Error handler (always LAST)
 app.use(errHandler);
-
