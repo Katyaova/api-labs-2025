44c00a0a01f1885060b2dfaa01aa5db3500d4906
Katyaova <puhhova@hotmail.com>
2025-04-30 10:20:29 +0100

    Added userId to tasks

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -6,10 +6,12 @@ import asyncHandler from 'express-async-handler';
 const router = express.Router(); 
 
 // Get all tasks
-router.get('/', async (req, res) => {
-    const tasks = await Task.find();
+// Get a user's tasks
+router.get('/user/:uid', async (req, res) => {
+    const tasks = await Task.find({ userId: `${req.params.uid}`});
     res.status(200).json(tasks);
 });
+
 
 // create a task
 router.post('/', asyncHandler(async (req, res) => {
